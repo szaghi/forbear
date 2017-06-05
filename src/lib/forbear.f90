0f5095755a93d52ff867d7a0716efe38c4aec0a6
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-06-05 14:21:47 +0200

    setup gh-pages

--- src/lib/forbear.f90
+++
@@ -1,9 +0,0 @@
-!< **forbear** project, Fortran (progress) B(e)ar environment.
-
-module forbear
-!< **forbear** project, Fortran (progress) B(e)ar environment.
-use forbear_bar_object, only : bar_object
-implicit none
-private
-public :: bar_object
-endmodule forbear
